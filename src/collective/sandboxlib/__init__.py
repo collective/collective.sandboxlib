43dd3aa7508f236e9f0010b18248faec846f1024
Jon Pentland <jon@iomedia.co.uk>
2015-06-30 11:55:48 +0100

    Add mime and plone.api.portal.get_tool to whitelist

--- src/collective/sandboxlib/__init__.py
+++ src/collective/sandboxlib/__init__.py
@@ -11,3 +11,19 @@ _ = MessageFactory('collective.sandboxlib')
 allow_module('pretaweb.plomino2pdf.api')
 pdf_api = ModuleSecurityInfo('pretaweb.plomino2pdf.api')
 pdf_api.declarePublic('generate_pdf')
+
+# Whitelist email functions
+allow_module('email.mime.multipart')
+multipart = ModuleSecurityInfo('email.mime.multipart')
+multipart.declarePublic('MIMEMultipart')
+allow_module('email.mime.text')
+text = ModuleSecurityInfo('email.mime.text')
+text.declarePublic('MIMEMultipart')
+allow_module('email.mime.application')
+application = ModuleSecurityInfo('email.mime.application')
+application.declarePublic('MIMEApplication')
+
+# Whitelist plone.api
+allow_module('plone.api.portal')
+portal = ModuleSecurityInfo('plone.api.portal')
+portal.declarePublic('get_tool')
